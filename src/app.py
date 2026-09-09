3fedf8c114bad9f55e043199640214978f2d652b
IselaBF <isela.barboza2005@gmail.com>
2026-09-09 08:25:43 -0600

    Actualiza app.py

--- src/app.py
+++ src/app.py
@@ -1,2 +1,13 @@
-mensaje = "Hola Mundo"
-print(mensaje)
+def percibe(trafico, velocidad, vehiculos):
+    # Percibe el tráfico, la velocidad y la cantidad de vehículos
+    return trafico, velocidad, vehiculos
+
+
+def decide(datos):
+    # Decide cómo ajustar los semáforos
+    return "ajustar semáforos"
+
+
+def acciona(ajuste):
+    # Modifica los tiempos de los semáforos y coordina el tránsito
+    return "mejorar circulación"
